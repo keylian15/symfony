4891d1492cc5feb8c730b2a8cbb1c2dc7c97864b
Keylian Turbé <keylian.turbe@univ-littoral.fr>
2024-12-20 12:51:29 +0100

    Ajout images

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,7 +8,10 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class ArticleController extends AbstractController
 {
@@ -21,8 +24,12 @@ class ArticleController extends AbstractController
     }
 
     #[Route('/article/creer', name: 'app_article_creer')]
-    public function create(EntityManagerInterface $entityManager, Request $request): Response
-    {
+    public function create(
+        EntityManagerInterface $entityManager,
+        Request $request,
+        SluggerInterface $slugger,
+        #[Autowire('%kernel.project_dir%/public/uploads/images')] string $imageDirectory
+    ): Response {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
         $article = new Article();
@@ -30,7 +37,26 @@ class ArticleController extends AbstractController
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $article = $form->getData();
+
+            $imageFile = $form->get('image')->getData();
+
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+
+                // Move the file to the directory where images are stored
+                try {
+                    $imageFile->move($imageDirectory, $newFilename);
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'imageFilename' property to store the PDF file name
+                // instead of its contents
+                $article->setImage($newFilename);
+            }
 
             $entityManager->persist($article);
             $entityManager->flush();
@@ -55,10 +81,14 @@ class ArticleController extends AbstractController
     }
 
     #[Route('/article/update/{id}', name: 'app_article_update')]
-    public function update(EntityManagerInterface $entityManager, int $id, Request $request): Response
-    {
+    public function update(
+        EntityManagerInterface $entityManager,
+        int $id,
+        Request $request,
+        SluggerInterface $slugger,
+        #[Autowire('%kernel.project_dir%/public/uploads/images')] string $imageDirectory
+    ): Response {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-
 
         if (!$id) {
             throw $this->createNotFoundException(
@@ -80,6 +110,27 @@ class ArticleController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $article = $form->getData();
+
+            $imageFile = $form->get('image')->getData();
+
+            // this condition is needed because the 'image' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+
+                // Move the file to the directory where images are stored
+                try {
+                    $imageFile->move($imageDirectory, $newFilename);
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                $article->setImage($newFilename);
+            }
+        
             $entityManager->persist($article);
             $entityManager->flush();
             $this->addFlash('success', 'Article modifier !');
